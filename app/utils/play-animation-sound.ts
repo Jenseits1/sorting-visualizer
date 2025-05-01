d738ff3f95f15e806acf3b8c1b59ad329b2df345
washingtonfbueno <washingtonfbueno@hotmail.com>
2025-05-01 15:59:35 -0300

    replaced animation sound by new

--- app/utils/play-animation-sound.ts
+++ app/utils/play-animation-sound.ts
@@ -11,36 +11,6 @@ const getAudioContext = () => {
 	return audioContext;
 };
 
-/* export const playAnimationSound = (number: number) => {
-	const context = getAudioContext();
-	if (!context) return;
-
-	const oscillator = context.createOscillator();
-	const gainNode = context.createGain();
-
-	oscillator.connect(gainNode);
-	gainNode.connect(context.destination);
-
-	oscillator.type = "sine";
-
-	const minFreq = 400;
-	const maxFreq = 800;
-	const frequency = minFreq + number * (maxFreq - minFreq);
-
-	oscillator.frequency.setValueAtTime(frequency, context.currentTime);
-
-	const duration = 0.05;
-
-	gainNode.gain.setValueAtTime(0.05, context.currentTime);
-	gainNode.gain.exponentialRampToValueAtTime(
-		0.0001,
-		context.currentTime + duration
-	);
-
-	oscillator.start(context.currentTime);
-	oscillator.stop(context.currentTime + duration);
-}; */
-
 export const playAnimationSound = (number: number) => {
 	const context = getAudioContext();
 	if (!context) return;
