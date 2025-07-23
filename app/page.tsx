52893ae4ea32574e2ea366eae1604ba155aad730
washingtonfbueno <washingtonfbueno@hotmail.com>
2025-07-23 15:43:18 -0300

    removed footer and updated main component

--- app/page.tsx
+++ app/page.tsx
@@ -6,27 +6,17 @@ import { SortControlComponent } from "./components/sort-control.component";
 import { NavbarComponent } from "./components/navbar.component";
 import { BarsCanvasComponent } from "./components/bars-canvas.component";
 import { ProgressBarComponent } from "./components/progress-bar.component";
-import { FooterComponent } from "./components/footer.component";
 
 export default function Home() {
 	return (
-		<Container
-			display="flex"
-			flexDirection="column"
-			justifyContent="space-between"
-			h="svh"
-		>
-			<Box spaceY="4" paddingY="8">
-				<NavbarComponent />
+		<Container h="svh" spaceY="4" paddingY="8">
+			<NavbarComponent />
 
-				<SortControlComponent />
+			<SortControlComponent />
 
-				<ProgressBarComponent />
+			<ProgressBarComponent />
 
-				<BarsCanvasComponent />
-			</Box>
-
-			<FooterComponent />
+			<BarsCanvasComponent />
 		</Container>
 	);
 }
